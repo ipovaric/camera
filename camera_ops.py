19b5939111f10c410ca39d2f310a6b67a06d42e1
Igor <ipovaric@gmail.com>
2023-07-18 20:26:53 -0600

    feat: gui to enable camera

--- camera_ops.py
+++ camera_ops.py
@@ -2,29 +2,26 @@
 
 import cv2
 
-vid = cv2.VideoCapture(0)
+def capture():
+    vid = cv2.VideoCapture(0)
 
-while(True):
+    while(True):
 
-    # Capture the video frame by frame
-    ret, frame = vid.read()
+        # Capture the video frame by frame
+        ret, frame = vid.read()
 
-    ret2, frame2 = vid.read()
-
-    # Display the resulting frame
-    cv2.imshow('frame', frame)
-
-    cv2.imshow('frame2', frame2)
-    
+        # Display the resulting frame
+        cv2.imshow('frame', frame)
 
 
-    # the 'q' button is set as the
-    # quitting button you may use any
-    # desired button of your choice
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
 
-# after loop release cap object
-vid.release()
+        # the 'q' button is set as the
+        # quitting button you may use any
+        # desired button of your choice
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-cv2.destroyAllWindows()
+    # after loop release cap object
+    vid.release()
+
+    cv2.destroyAllWindows()
--- gui.py
+++ gui.py
@@ -1,1 +1,13 @@
-from tkinte r
+import tkinter as tk
+from tkinter import ttk
+
+
+window = tk.Tk()
+window.geometry('200x100')
+
+button = ttk.Button(
+    window,
+    text='Run Camera',
+    width = 25)
+button.pack()
+window.mainloop()
